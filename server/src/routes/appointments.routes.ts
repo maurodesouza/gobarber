a3362218517629dc43ad781e4ab7ed987461c1a2
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-08-28 20:36:45 -0300

    📘  (server) class 4 - validate date

--- server/src/routes/appointments.routes.ts
+++ server/src/routes/appointments.routes.ts
@@ -1,17 +1,36 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { parseISO, startOfHour, isEqual } from 'date-fns';
+
+interface Appointment {
+  provider: string;
+  id: string;
+  date: Date;
+}
 
 const routes = Router();
 
-const appointments = [];
+const appointments: Appointment[] = [];
 
 routes.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ error: 'Já existe um agendamento nesse horário' });
+  }
+
   const newAppointment = {
-    provider,
-    date,
     id: uuid(),
+    provider,
+    date: parsedDate,
   };
 
   appointments.push(newAppointment);
