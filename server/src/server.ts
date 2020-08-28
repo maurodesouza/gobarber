57fa346121a7047b519b0597ca0a5f7aa8b1045a
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-08-28 20:04:23 -0300

    📗  (server) class 2 - style guide

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,8 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Ahhh .. Hellow??' });
-});
+app.use(routes);
 
 app.listen(3333);
