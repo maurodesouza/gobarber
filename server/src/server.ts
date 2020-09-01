3be53e2b2837ffae0dfa1275d6dcac7833771622
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-09-01 20:53:51 -0300

    📘  (server) class 23 - serving static files

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,9 +4,13 @@ import './database';
 import express from 'express';
 import routes from './routes';
 
+import uploadConfig from './config/upload';
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333);
