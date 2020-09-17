b2011d0c7a9b4699b4a987c6c64644a76dc03cf2
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-09-17 01:01:52 -0300

    📒  (web) class 3 - global styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import GlobalStyles from './styles/global';
 
-function App() {
-  return <h1>Ahhh ... Hellow??</h1>;
-}
+const App = () => {
+  return (
+    <>
+      <h1>Ahhh ... Hellow??</h1>
+      <GlobalStyles />
+    </>
+  );
+};
 
 export default App;
