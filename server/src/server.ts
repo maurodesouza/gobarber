e274b811dc46087c4b6b1f0b181fe6a11c6bb77c
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-09-19 17:48:14 -0300

    📒  (server) class 26 - cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,12 +4,15 @@ import './database';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
+import cors from 'cors';
+
 import AppError from './error/AppError';
 import routes from './routes';
 import uploadConfig from './config/upload';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/files', express.static(uploadConfig.directory));
