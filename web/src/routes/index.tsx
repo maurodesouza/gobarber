c3aca5d0aa32675fef64c983a80c76db906225ec
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-09-20 22:42:45 -0300

    📘  (web) class 23 - authenticated routes

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
 
 const Routes = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" exact component={SignUp} />
+
+    <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
 );
 
