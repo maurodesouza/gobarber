03d778365de7068fcc6f24272f004f8ba4da766b
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-09-25 10:47:53 -0300

    📗  (mobile) class 2 - style guide

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +17,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
